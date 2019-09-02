7ace1917a6a04930bfd7de3e9505380ba79996c8
gigimon <oleg@scalr.com>
2019-09-02 16:41:16 +0300

    * more code

--- aws/main.tf
+++ aws/main.tf
@@ -4,10 +4,18 @@ provider "aws" {
   secret_key = "${var.scalr_aws_secret_key}"
 }
 
-
+resource "random_id" "server_id" {
+  byte_length = 4
+}
 
 resource "aws_instance" "oleg-tf" {
-  ami = "${var.image_id}"
-  instance_type = "${var.instance_type}"
-  subnet_id = "${var.subnet_id}"
+  name = "instancename"
+  ami = var.image_id
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+
+  tags = {
+    Name = "oleg-${random_id.server_id.hex}"
+    owner = "oleg"
+  }
 }
