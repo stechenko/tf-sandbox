45ba973158a543f2208c8929ec191f2ade50676f
Oleg <oleg@scalr.com>
2019-11-12 17:21:35 +0300

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -12,4 +12,9 @@ resource "aws_instance" "oleg-tf" {
   ami = var.image_id
   instance_type = var.instance_type
   subnet_id = var.subnet_id
+  
+  tags = {
+#    Name = "oleg-${random_id.server_id.hex}"
+    owner = "oleg"
+  }
 }
