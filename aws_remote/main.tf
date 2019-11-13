9a89077d62f5ef7e9dfe49ed19bb0ffaacbc1341
gigimon <oleg@scalr.com>
2019-11-13 11:29:24 +0300

    * add example of aws_remote

---
+++ aws_remote/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "remote" {
+    hostname = ""
+    organization = "org-sgpvfvrkj5ao2j0"
+    workspaces {
+      name = "testopa"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  access_key = var.scalr_aws_access_key
+  secret_key = var.scalr_aws_secret_key
+}
+
+resource "random_id" "server_id" {
+  byte_length = 4
+}
+
+resource "aws_instance" "oleg-tf" {
+  ami = var.image_id
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+
+  tags = {
+    Name = "oleg-${random_id.server_id.hex}"
+    owner = "oleg"
+  }
+}
---
+++ aws_remote/variables.tf
@@ -0,0 +1,26 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "image_id" {
+  type = string
+  default = "ami-07d0cf3af28718ef8"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.small"
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "scalr_aws_access_key" {
+  description = "AWS Access Key (sensitive)"
+}
+
+variable "scalr_aws_secret_key" {
+  description = "AWS Secret Key (sensitive)"
+}
