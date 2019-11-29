c2b5f84bab7cdeae9a75fbde640565e24f8a9e65
gigimon <oleg@scalr.com>
2019-11-29 14:53:30 +0300

    * add azure

---
+++ azure/main.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  client_id = var.scalr_azurerm_client_id
+  client_secret = var.scalr_azurerm_client_secret
+  subscription_id = var.scalr_azurerm_subscription_id
+  tenant_id = var.scalr_azurerm_tenant_id
+  environment = var.scalr_azurerm_environment
+}
+
+resource "azurerm_virtual_machine" "oleg1" {
+  location = "East US"
+  name = "olegtf"
+  network_interface_ids = ["revizor"]
+  resource_group_name = "revizor"
+  vm_size = "Basic_A0"
+
+  storage_os_disk {
+    create_option = "FromImage"
+    name = "olegdisk"
+  }
+}
---
+++ azure/variables.tf
@@ -0,0 +1,19 @@
+variable "scalr_azurerm_client_id" {
+
+}
+
+variable "scalr_azurerm_client_secret" {
+
+}
+
+variable "scalr_azurerm_environment" {
+
+}
+
+variable "scalr_azurerm_subscription_id" {
+
+}
+
+variable "scalr_azurerm_tenant_id" {
+
+}
