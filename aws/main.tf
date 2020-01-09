c4f1f51fd5581fdf9ca275dc46b95e2591b69293
gigimon <oleg@scalr.com>
2020-01-09 13:32:16 +0300

    * check policy fail

--- aws/main.tf
+++ aws/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "oleg-tf" {
   instance_type = var.instance_type
   subnet_id = var.subnet_id
   tags = {
-#      Name = "oleg-${random_id.server_id.hex}"
+      Name = "oleg-${random_id.server_id.hex}"
       owner = "oleg"
   }
 }
